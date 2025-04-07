89bd900c791f4493059d2aa66ae5b6bb42648ca5
Matheus Correia <matheuscstech@gmail.com>
2025-04-07 19:12:12 -0300

    Exercícios Práticos em JavaScript

---
+++ Aula 11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'brasil'
+console.log(`vivendo em ${país}` )
+if (país == 'Brasil' || 'brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é um Estrangeiro!')
+} 
+ 
